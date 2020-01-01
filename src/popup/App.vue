0c3e467ba7064ce0a87dd07d2eb5992c3da9151c
gaoliang <gaoliangim@gmail.com>
2020-01-02 01:37:42 +0800

    feat: 修改列表样式 & 支持全部删除

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -10,6 +10,9 @@
       <v-toolbar-title>Cookie Cook</v-toolbar-title>
       <v-spacer></v-spacer>
 
+      <v-btn icon color="error" @click="deleteCookies">
+        <v-icon>mdi-delete-forever</v-icon>
+      </v-btn>
       <v-btn icon>
         <v-icon>mdi-export</v-icon>
       </v-btn>
@@ -25,33 +28,46 @@
     <v-content>
       <!-- Provides the application the proper gutter -->
       <v-container fluid>
-        <v-data-table
-          :headers="headers"
-          :items="cookies"
-          :items-per-page="5"
-          class="elevation-1 cookies-table"
-        >
-          <template v-slot:item.hostOnly="{ item }">
-            <v-checkbox v-model="item.hostOnly"></v-checkbox>
-          </template>
-          <template v-slot:item.secure="{ item }">
-            <v-checkbox v-model="item.secure"></v-checkbox>
-          </template>
-          <template v-slot:item.httpOnly="{ item }">
-            <v-checkbox v-model="item.httpOnly"></v-checkbox>
-          </template>
-          <template v-slot:item.session="{ item }">
-            <v-checkbox v-model="item.session"></v-checkbox>
-          </template>
-          <template v-slot:item.value="{ item }">
-            <v-text-field hide-details v-model="item.value"></v-text-field>
-          </template>
-        </v-data-table>
+        <v-expansion-panels>
+          <v-expansion-panel v-for="(cookie,i) in cookies" :key="i">
+            <v-expansion-panel-header>
+              <v-row no-gutters>
+                <v-col cols="4" class="text--secondary">{{ cookie.domain }}{{cookie.path}}</v-col>
+                <v-col cols="8">{{cookie.name}}</v-col>
+              </v-row>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-row no-gutters>
+                <v-col cols="12" class="text--secondary">
+                  <v-textarea label="Value" v-model="cookie.value" hide-details auto-grow rows="1"></v-textarea>
+                </v-col>
+                <v-col cols="6" class="text--secondary">
+                  <v-textarea label="Path" v-model="cookie.path" hide-details auto-grow rows="1"></v-textarea>
+                </v-col>
+                <v-col cols="6" class="text--secondary">
+                  <v-select :items="sameSiteItems" label="SameSite" v-model="cookie.sameSite"></v-select>
+                </v-col>
+                <v-col cols="3">
+                  <v-switch v-model="cookie.hostOnly" label="HostOnly"></v-switch>
+                </v-col>
+                <v-col cols="3">
+                  <v-switch v-model="cookie.httpOnly" label="HttpOnly"></v-switch>
+                </v-col>
+                <v-col cols="3">
+                  <v-switch v-model="cookie.secure" label="Secure"></v-switch>
+                </v-col>
+                <v-col cols="3">
+                  <v-switch v-model="cookie.session" label="Session"></v-switch>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
       </v-container>
     </v-content>
 
     <v-footer app>
-      <v-btn icon>
+      <v-btn icon @click="openIndex">
         <v-icon>mdi-github-circle</v-icon>
       </v-btn>
     </v-footer>
@@ -63,31 +79,26 @@
 export default {
   data () {
     return {
-      headers: [
-        {
-          text: 'Name',
-          align: 'left',
-          sortable: false,
-          value: 'name'
-        },
-        { text: 'Value', value: 'value', sortable: false },
-        { text: 'Domain', value: 'domain', sortable: false },
-        { text: 'HostOnly', value: 'hostOnly', sortable: false },
-        { text: 'HttpOnly', value: 'httpOnly', sortable: false },
-        { text: 'Session', value: 'session', sortable: false },
-        { text: 'Secure', value: 'secure', sortable: false }
-
-      ],
       currentTabID: null,
       currentTabURL: '',
       currentTabCookiesStoreId: null,
-      cookies: []
+      cookies: [],
+      sameSiteItems: ['strict', 'lax', 'none', 'no_restriction', 'unspecified']
     }
   },
   mounted () {
+    if ((window.matchMedia('(prefers-color-scheme: light)').matches) ||
+      (window.matchMedia('(prefers-color-scheme: no-preference)').matches)) {
+      this.$vuetify.theme.dark = false
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      this.$vuetify.theme.dark = true
+    }
     this.init()
   },
   methods: {
+    openIndex () {
+      chrome.tabs.create({ url: 'https://www.github.com/gaoliang/cookie-cook' })
+    },
     searchCookies () {
       let that = this
       chrome.cookies.getAllCookieStores(function (cookieStores) {
@@ -116,6 +127,19 @@ export default {
           that.searchCookies()
         }
       )
+    },
+    deleteCookies () {
+      let that = this
+      this.cookies.forEach(function (cookie) {
+        chrome.cookies.remove(
+          {
+            'url': that.currentTabURL,
+            'name': cookie.name,
+            'storeId': cookie.storeId
+          }, function () {
+            that.init()
+          })
+      })
     }
   },
   name: 'App'
